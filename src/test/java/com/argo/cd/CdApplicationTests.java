896e5ae600590e6a732be39a4a52b3aa08ea660b
sokpanharith <sokpanharith.dev@gmail.com>
2025-08-30 16:46:16 +0700

    project init

---
+++ src/test/java/com/argo/cd/CdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argo.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
